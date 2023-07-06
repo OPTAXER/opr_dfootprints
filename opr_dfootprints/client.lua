f553c43bdf477e8367ecb413776347d9bf48e9f8
Optaxer | OPR <84898728+OPTAXER@users.noreply.github.com>
2023-07-06 14:02:21 +0530

    v1

---
+++ opr_dfootprints/client.lua
@@ -0,0 +1,8 @@
+Citizen.CreateThread(function()
+    while true
+    do
+    SetForcePedFootstepsTracks(false)
+    SetForceVehicleTrails(false)
+    Citizen.Wait(1) --optimize it to your likings
+    end
+    end)
---
+++ opr_dfootprints/fxmanifest.lua
@@ -0,0 +1,7 @@
+fx_version 'cerulean'
+
+game 'gta5'
+
+description 'Opr custom script to disable footprints'
+
+client_script 'client.lua'
